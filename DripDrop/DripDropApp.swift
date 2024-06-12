b2dca803a49a2af7f0ad413da2d7131dcf30f3cd
Michael-Espineli <michael@espinelicapital.com>
2024-06-11 19:49:18 -0700

    Initial Commit

---
+++ DripDrop/DripDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripDropApp.swift
+//  DripDrop
+//
+//  Created by Michael Espineli on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DripDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
