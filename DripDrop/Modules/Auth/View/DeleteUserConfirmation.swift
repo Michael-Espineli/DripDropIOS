bfc0cd580c1c11e91772d526e34a8046318ece18
Michael-Espineli <michael@espinelicapital.com>
2026-05-13 07:09:56 -0700

    Pre may release

---
+++ DripDrop/Modules/Auth/View/DeleteUserConfirmation.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteUserConfirmation.swift
+//  DripDrop
+//
+//  Created by Michael Espineli on 5/4/26.
+//
+
+import SwiftUI
+
+struct DeleteUserConfirmation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteUserConfirmation()
+}
